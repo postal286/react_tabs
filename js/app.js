b270f5cec421a4f9566bfb57251fc0eebea42b2d
Apostol <kovalev333@mail.ru>
2017-06-09 15:32:09 +0600

    display components

---
+++ js/app.js
@@ -0,0 +1,109 @@
+var dataTabs = [
+	{
+		title: 'Some Title',
+		text: 'Some text',
+		tabId: '0'
+	},
+	{
+		title: 'Some Second Title',
+		text: 'Some second text',
+		tabId: '1'
+	},
+	{
+		title: 'Some Third Title',
+		text: 'Some third text',
+		tabId: '2'
+	}
+];
+
+var Tab = React.createClass({
+	getInitialState: function () {
+		return ({
+			active: ''
+		});
+	},
+
+	componentDidMount: function () {
+
+	},
+
+	render: function () {
+
+		var data = this.props.data;
+		var dataTemplate;
+
+		if (data.length > 0) {
+			dataTemplate = data.map(function(item, index) {
+				return (
+					<li key={item.tabId}>
+						<a className={'tab-' + index}>
+							{item.title}
+						</a>
+					</li>
+				);
+			});
+		} else {
+			dataTemplate = 'Извините, но ничего пока нет.'
+		}
+		return(
+			<ul className='tab'>
+				{dataTemplate}
+			</ul>
+		);
+	}
+});
+
+var Content = React.createClass({
+
+	render: function () {
+
+		var data = this.props.data;
+		var ContentTemplate;
+
+		if (data.length > 0) {
+			ContentTemplate = data.map(function(item, index) {
+				return (
+					<div key={index}>
+						<div className={'tab-content content' + index}>
+							<h3>{item.title}</h3>
+							<p>{item.text}</p>
+						</div>
+					</div>
+				);
+			});
+		} else {
+			ContentTemplate = 'Извините, но ничего пока нет.'
+		}
+		return(
+			<div>
+				{ContentTemplate}
+			</div>
+		);
+	}
+});
+
+var Tabs = React.createClass({
+	propTypes: {
+		data: React.PropTypes.array
+	},
+
+	getInitialState: function () {
+		return ({
+			data: dataTabs
+		});
+	},
+
+	render: function () {
+		return (
+			<div>
+				<Tab data={this.state.data}/>
+				<Content data={this.state.data}/>
+			</div>
+		);
+	}
+});
+
+ReactDOM.render(
+	<Tabs />,
+	document.getElementById('root')
+);
