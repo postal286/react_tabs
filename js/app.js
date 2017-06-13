081960265f3d9c28a22e17bd02c5b79db01b2ea7
Apostol <kovalev333@mail.ru>
2017-06-14 01:06:50 +0600

    tabs done

--- js/app.js
+++ js/app.js
@@ -1,38 +1,55 @@
 var dataTabs = [
 	{
-		title: 'Some Title',
-		text: 'Some text',
+		title: 'London',
+		text: 'Some text about London',
 		tabId: '0'
 	},
 	{
-		title: 'Some Second Title',
-		text: 'Some second text',
+		title: 'Moscow',
+		text: 'Some text about Moscow',
 		tabId: '1'
 	},
 	{
-		title: 'Some Third Title',
-		text: 'Some third text',
+		title: 'Paris',
+		text: 'Some text about Paris',
 		tabId: '2'
 	}
 ] || [];
+
+var TabArrow = React.createClass({
+	onTabClick: function () {
+		this.props.onTabClick(this.props.item.title, this.props.item.text);
+	},
+	render: function () {
+		return (
+			<a className={this.props.className} onClick={this.onTabClick} >
+				{this.props.item.title}
+			</a>
+		);
+	}
+});
 
 var Tab = React.createClass({
 
 	render: function () {
 
-		var data = this.props.data;
-		var dataTemplate;
+		var data  = this.props.dataTab,
+				dataTemplate;
 
 		if (data.length > 0) {
 			dataTemplate = data.map(function(item, index) {
 				return (
 					<li key={item.tabId}>
-						<a className={'tab-' + index}>
-							{item.title}
-						</a>
+						<TabArrow
+						item={item}
+						className={'tab-' + index}
+						onTabClick={this.props.changeTabContent}
+						/>
 					</li>
 				);
-			});
+			},
+			this
+			);
 		} else {
 			dataTemplate = 'Извините, но ничего пока нет.'
 		}
@@ -45,50 +62,44 @@ var Tab = React.createClass({
 });
 
 var Content = React.createClass({
-
 	render: function () {
+		var title = this.props.dataTitle,
+				text  = this.props.dataText;
 
-		var data = this.props.data;
-		var ContentTemplate;
-
-		if (data.length > 0) {
-			ContentTemplate = data.map(function(item) {
-				return (
-					<div key={item.tabId}>
-						<div className={'tab-content'}>
-							<h3>{item.title}</h3>
-							<p>{item.text}</p>
-						</div>
-					</div>
-				);
-			});
-		} else {
-			ContentTemplate = 'Извините, но ничего пока нет.'
-		}
 		return(
-			<div>
-				{ContentTemplate}
+			<div className='tab-content'>
+				<div>{title}</div>
+				<div>{text}</div>
 			</div>
 		);
 	}
 });
 
 var Tabs = React.createClass({
-	propTypes: {
-		data: React.PropTypes.array
+	changeTabContent: function (newTitle, newText) {
+		this.setState({
+			title: newTitle,
+			text: newText
+		})
 	},
 
 	getInitialState: function () {
 		return ({
-			data: dataTabs
+			data: dataTabs,
+			title: dataTabs[0].title,
+			text: dataTabs[0].text,
 		});
 	},
 
 	render: function () {
 		return (
 			<div>
-				<Tab data={this.state.data}/>
-				<Content data={this.state.data}/>
+				<Tab dataTab={this.state.data}
+						 changeTabContent={this.changeTabContent}
+				/>
+				<Content dataTitle={this.state.title}
+								 dataText={this.state.text}
+				/>
 			</div>
 		);
 	}
